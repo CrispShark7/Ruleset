bcc8e71621e7600ffa9bb2840d0c8ff81702955a
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2025-12-26 16:10:23 +0800

    更新 Build.sh

--- Build.sh
+++ Build.sh
@@ -4,25 +4,21 @@ set -euo pipefail
 
 repository="$(basename "$GITHUB_REPOSITORY")"
 
+echo "Execute in $repository Repository"
+
 rm -rf ios_rule_script
 git clone -q https://github.com/blackmatrix7/ios_rule_script.git ios_rule_script
 
-echo "Execute in $repository Repository"
-
-rm -rf "$repository"/Egern/* "$repository"/Singbox/*
+rm -rf "$repository"/{Egern,Singbox}
 mkdir -p "$repository"/{Egern,Singbox}
 
 while IFS= read -r -d '' file; do
     rel_path="${file#ios_rule_script/rule/Clash/}"
-
-    dest="$repository/Egern/${rel_path%.list}.yaml"
-    mkdir -p "$(dirname "$dest")"
-    cp "$file" "$dest"
-
-    dest="$repository/Singbox/${rel_path%.list}.json"
-    mkdir -p "$(dirname "$dest")"
-    cp "$file" "$dest"
-done < <(find ios_rule_script/rule/Clash -type f -name "*.list" -print0)
+    rel_dir="$(dirname "$rel_path")"
+    mkdir -p "$repository/Egern/$rel_dir" "$repository/Singbox/$rel_dir"
+    cp "$file" "$repository/Egern/${rel_path%.list}.yaml"
+    cp "$file" "$repository/Singbox/${rel_path%.list}.json"
+done < <(find ios_rule_script/rule/Clash -mindepth 1 -type f -name "*.list" -print0)
 
 echo "Copied and renamed all rules to $repository/Egern and $repository/Singbox"
 echo "$repository Repository: All Ruleset Processed!"
