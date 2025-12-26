b071f3b96da0633963fbd09b71bd36b26d8c3d80
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2025-12-26 15:35:16 +0800

    更新 Build.sh

--- Build.sh
+++ Build.sh
@@ -3,14 +3,20 @@
 set -euo pipefail
 
 repository="$(basename "$GITHUB_REPOSITORY")"
-rm -rf ios_rule_script
-git clone -q https://github.com/blackmatrix7/ios_rule_script.git ios_rule_script
 
-if [[ "$repository" == "Ruleset" ]]; then
-    echo "Execute in $repository Repository"
-    mkdir -p "$repository"/{Egern,Singbox}
-    cp -r ios_rule_script/rule/Clash/* "$repository"/Egern/
-    cp -r ios_rule_script/rule/Clash/* "$repository"/Singbox/
-    echo "Copied All Rule to $repository/Egern and $repository/Singbox"
-    echo "$repository Repository: All Ruleset Processed!"
-fi
+
+rm -rf ios_rule_script
+curl -sL https://github.com/blackmatrix7/ios_rule_script/archive/refs/heads/master.zip -o master.zip
+unzip -q master.zip
+mv ios_rule_script-master ios_rule_script
+rm master.zip
+
+echo "Execute in $repository Repository"
+mkdir -p "$repository"/{Egern,Singbox}
+
+# 一次性复制 Clash 下所有内容到 Egern 和 Singbox
+cp -r ios_rule_script/rule/Clash/* "$repository"/Egern/
+cp -r ios_rule_script/rule/Clash/* "$repository"/Singbox/
+
+echo "Copied All Rule to $repository/Egern and $repository/Singbox"
+echo "$repository Repository: All Ruleset Processed!"
