67e00e1b9e07b971c3c799bb835445aebf482dc1
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2025-12-26 15:58:33 +0800

    更新 Build.sh

--- Build.sh
+++ Build.sh
@@ -9,15 +9,20 @@ git clone -q https://github.com/blackmatrix7/ios_rule_script.git ios_rule_script
 
 echo "Execute in $repository Repository"
 
+# 创建目标目录
 mkdir -p "$repository"/{Egern,Singbox}
 
+# 递归处理 .list 文件，同时复制并改名
 while IFS= read -r -d '' file; do
+    # 获取相对路径
     rel_path="${file#ios_rule_script/rule/Clash/}"
-
+    
+    # 目标路径：Egern -> .yaml
     dest="$repository/Egern/${rel_path%.list}.yaml"
     mkdir -p "$(dirname "$dest")"
     cp "$file" "$dest"
 
+    # 目标路径：Singbox -> .json
     dest="$repository/Singbox/${rel_path%.list}.json"
     mkdir -p "$(dirname "$dest")"
     cp "$file" "$dest"
