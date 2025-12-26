abf8eb48832549cbe9f527c2f437edbc8367b458
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2025-12-26 15:37:10 +0800

    更新 Build.sh

--- Build.sh
+++ Build.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 repository="$(basename "$GITHUB_REPOSITORY")"
 
-
 rm -rf ios_rule_script
 curl -sL https://github.com/blackmatrix7/ios_rule_script/archive/refs/heads/master.zip -o master.zip
 unzip -q master.zip
@@ -14,7 +13,6 @@ rm master.zip
 echo "Execute in $repository Repository"
 mkdir -p "$repository"/{Egern,Singbox}
 
-# 一次性复制 Clash 下所有内容到 Egern 和 Singbox
 cp -r ios_rule_script/rule/Clash/* "$repository"/Egern/
 cp -r ios_rule_script/rule/Clash/* "$repository"/Singbox/
 
