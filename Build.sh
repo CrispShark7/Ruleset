c53d884b4d33f18a075cc5e922489ca33e7bb21e
CrispShark7 <144424283+CrispShark7@users.noreply.github.com>
2025-12-26 15:27:26 +0800

    更新 Build.sh

--- Build.sh
+++ Build.sh
@@ -3,18 +3,14 @@
 set -euo pipefail
 
 repository="$(basename "$GITHUB_REPOSITORY")"
-
 rm -rf ios_rule_script
-
 git clone -q https://github.com/blackmatrix7/ios_rule_script.git ios_rule_script
 
 if [[ "$repository" == "Ruleset" ]]; then
     echo "Execute in $repository Repository"
-    rule_dirs=("Egern" "Singbox")
-    for rule_path in "${rule_dirs[@]}"; do
-        mkdir -p "$repository/$rule_path"
-        cp -r ios_rule_script/rule/Clash/* "$repository/$rule_path/"
-        echo "Copied all rules to $repository/$rule_path"
-    done
+    mkdir -p "$repository"/{Egern,Singbox}
+    cp -r ios_rule_script/rule/Clash/* "$repository"/Egern/
+    cp -r ios_rule_script/rule/Clash/* "$repository"/Singbox/
+    echo "Copied All Rule to $repository/Egern and $repository/Singbox"
     echo "$repository Repository: All Ruleset Processed!"
 fi
